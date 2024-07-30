4aacfec1ef8ff89d8713e140a29e2afe4b35ab81
kalpshah_simform <kalp.s@simformsolutions.com>
2024-07-30 18:04:20 +0530

    completed search input and categories selection filter

--- src/app/(dashboard)/(routes)/search/page.tsx
+++ src/app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { db } from "@/lib/db";
+import React from "react";
+import { Categories } from "./_components/categories";
+import { SearchInput } from "@/components/search-input";
 
-export default function SearchPage() {
+export default async function SearchPage() {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
   return (
-    <div>search</div>
-  )
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
+    </>
+  );
 }
--- src/components/navbar-routes.tsx
+++ src/components/navbar-routes.tsx
@@ -8,45 +8,53 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 export default function NavbarRoutes() {
   const pathname = usePathname();
-  const router = useRouter();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
+  const isSearchPage = pathname === "/search";
 
   return (
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacherPage || isPlayerPage ? (
-        <Link href="/">
-          <Button size="sm" variant="ghost">
-            <LogOut className="h-4 w-4 mr-2" />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button size="sm" variant="ghost">
-            Teacher mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
       )}
-      <ClerkLoading>
-        <div>Clerk is loading...</div>
-      </ClerkLoading>
-      <ClerkLoaded>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton afterSignOutUrl="/" />
-        </SignedIn>
-      </ClerkLoaded>
-    </div>
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacherPage || isPlayerPage ? (
+          <Link href="/">
+            <Button size="sm" variant="ghost">
+              <LogOut className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              Teacher mode
+            </Button>
+          </Link>
+        )}
+        <ClerkLoading>
+          <div>Clerk is loading...</div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+        </ClerkLoaded>
+      </div>
+    </>
   );
 }
