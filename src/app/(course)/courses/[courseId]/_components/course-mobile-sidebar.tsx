9ddf1b35fc7b238513d9c2d9e01fc171680fdded
kalpshah_simform <kalp.s@simformsolutions.com>
2024-10-13 19:39:22 +0530

    feat: added chapterId page with videoplayer

--- src/app/(course)/courses/[courseId]/_components/course-mobile-sidebar.tsx
+++ src/app/(course)/courses/[courseId]/_components/course-mobile-sidebar.tsx
@@ -18,7 +18,7 @@ export const CourseMobileSidebar = ({
 }: CourseMobileSidebarProps) => {
   return (
     <Sheet>
-      <SheetTrigger className="md:hiddeb pr-4 hover:opacity-75 transition">
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-white w-72">
